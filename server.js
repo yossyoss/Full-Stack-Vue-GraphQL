dd70835243005661f730b81928ea7829fc3117cc
yossyoss <yosi0679@gmail.com>
2018-09-16 01:19:14 +0300

    adding apollo-server and mongoDB

---
+++ server.js
@@ -0,0 +1,30 @@
+const { ApolloServer, gql } = require("apollo-server");
+const mongoose = require("mongoose");
+require("dotenv").config({ path: "variables.env" });
+
+mongoose
+  .connect(
+    process.env.MONGO_URI,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("DB connected"))
+  .catch(err => console.error(err));
+
+const typeDefs = gql`
+  type Todo {
+    task: String
+    completed: Boolean
+  }
+
+  type Query {
+    getTodos: [Todo]
+  }
+`;
+
+const server = new ApolloServer({
+  typeDefs
+});
+
+server.listen().then(({ url }) => {
+  console.log(`server listening on ${url}`);
+});
