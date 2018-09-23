794c2daa7c3dc07758c28db22bb759b9c240afcf
yossyoss <yosi0679@gmail.com>
2018-09-23 11:19:31 -0700

    adding add post method

--- resolvers.js
+++ resolvers.js
@@ -1,19 +1,33 @@
 module.exports = {
-    Query: {
-        getUser: () => null
+  Query: {
+    getUser: () => null
+  },
+  Mutation: {
+    addPost: async (
+      _,
+      { title, imageUrl, categories, description, creatorId },
+      { Post }
+    ) => {
+      const newPost = await new Post({
+        title,
+        imageUrl,
+        categories,
+        description,
+        createdBy: creatorId
+      }).save();
+      return newPost;
     },
-    Mutation: {
-        signupUser: async (_, {username,email, password}, {User}) => {
-            const user = await User.findOne({ username });
-            if(user) {
-                throw new Error('user already exist');
-            }
-            const newUser = await new User({
-                username,
-                email,
-                password
-            }).save();
-            return newUser;
-        }
+    signupUser: async (_, { username, email, password }, { User }) => {
+      const user = await User.findOne({ username });
+      if (user) {
+        throw new Error("user already exist");
+      }
+      const newUser = await new User({
+        username,
+        email,
+        password
+      }).save();
+      return newUser;
     }
-}
+  }
+};
