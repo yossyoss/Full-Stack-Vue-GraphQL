1e3244668b6873dd9b619c5d200a3a0dee54f62e
yossyoss <yosi0679@gmail.com>
2018-10-08 00:06:34 +0300

    making a query

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,12 +1,13 @@
 <template>
   <v-container>
     <h1>Home</h1>
-    <!-- <ul v-for="post in getPosts" :key="post._id">
+    <div v-if="$apollo.loading">Loading...</div>
+    <ul v-else v-for="post in getPosts" :key="post._id">
       <li>
         {{post.title}} {{post.imageUrl}} {{post.description}}
       </li>
       <li>{{post.likes}}</li>
-    </ul> -->
+    </ul>
   </v-container>
 </template>
 
@@ -14,21 +15,36 @@
 import { gql } from "apollo-boost";
 
 export default {
-  name: "home"
-  // apollo: {
-  //   getPosts: {
-  //     query: gql`
-  //       query {
-  //         getPosts {
-  //           _id
-  //           title
-  //           imageUrl
-  //           description
-  //           likes
-  //         }
-  //       }
-  //     `
-  //   }
-  // }
+  name: "home",
+  data() {
+    return {
+      posts: []
+    };
+  },
+  apollo: {
+    getPosts: {
+      query: gql`
+        query {
+          getPosts {
+            _id
+            title
+            imageUrl
+            description
+            likes
+          }
+        }
+      `,
+      result({ data, loading, networkStatus }) {
+        if (!loading) {
+          this.posts = data.getPosts;
+          console.log("[networkStatus]", networkStatus); //7 means that it's working fine
+        }
+      },
+      error(err){
+        console.error('[ERROR!!]',err);
+        console.dir(err)
+      }
+    }
+  }
 };
 </script>
