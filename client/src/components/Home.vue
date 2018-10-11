c975d41d5c64d198f2cab7263555f9cba8fa9e1b
yossyoss <yosi0679@gmail.com>
2018-10-11 19:29:24 +0300

    adding carousel image

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -1,18 +1,13 @@
 <template>
-  <v-container>
-    <ApolloQuery :query="getPostsQuery">
-      <template slot-scope="{ result: {loading, error, data, networkStatus} }">
-        <div v-if="loading">Loading...</div>
-        <div v-else-if="error">Error! {{error.message}}</div>
-        <div v-else-if="!loading">Network Status: {{networkStatus}}</div>
-        <ul v-else v-for="post in data.getPosts" :key="post._id">
-          <li>
-            {{post.title}} {{post.imageUrl}} {{post.description}}
-          </li>
-          <li>{{post.likes}}</li>
-        </ul>
-      </template>
-    </ApolloQuery>
+  <v-container text-xs-center v-if="getPosts">
+    <v-flex xs12>
+      <v-carousel v-bind="{ 'cycle': true}" interval="3000">
+        <v-carousel-item v-for="post in getPosts" :key="post._id" :src="post.imageUrl">
+          <h1 id="carousel__title">{{post.title}}</h1>
+        </v-carousel-item>
+      </v-carousel>
+    </v-flex>
+
   </v-container>
 </template>
 
@@ -23,7 +18,12 @@ export default {
   name: "home",
   data() {
     return {
-      getPostsQuery: gql`
+      posts: []
+    };
+  },
+  apollo: {
+    getPosts: {
+      query: gql`
         query {
           getPosts {
             _id
@@ -33,8 +33,31 @@ export default {
             likes
           }
         }
-      `
-    };
+      `,
+      result({ data, loading, networkStatus }) {
+        if (!loading) {
+          this.posts = data.getPosts;
+          console.log("[networkStatus]", networkStatus); //7 means that it's working fine
+        }
+      },
+      error(err) {
+        console.error("[ERROR!!]", err);
+        console.dir(err);
+      }
+    }
   }
 };
 </script>
+<style>
+#carousel__title {
+  color: #fff;
+  background-color: rgba(0, 0, 0, 0.5);
+  position: absolute;
+  border-radius: 5px 5px 0 0;
+  padding: 0.5em;
+  margin: 0 auto;
+  bottom: 50px;
+  left: 0;
+  right: 0;
+}
+</style>
