684991f87c6f13c53582f3d3202186a4e2626329
yossyoss <yosi0679@gmail.com>
2018-10-11 20:39:47 +0300

    hashing password and adding avatar

--- models/User.js
+++ models/User.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
-
+const md5 = require("md5");
+const bcrypt = require("bcrypt");
 const UserSchema = new mongoose.Schema({
   username: {
     type: String,
@@ -29,6 +30,27 @@ const UserSchema = new mongoose.Schema({
     required: true,
     ref: "Post"
   }
+});
+
+//Create and add avatar to user
+UserSchema.pre("save", function(next) {
+  this.avatar = `http://gravatar.com/avatar/${md5(this.username)}?d=identicon`;
+  next();
+});
+
+//Hash password so it can't be seen
+UserSchema.pre("save", function(next) {
+  if (!this.isModified("password")) {
+    return next();
+  }
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err);
+    bcrypt.hash(this.password, salt, (err, hash) => {
+      if (err) return next(err);
+      this.password = hash;
+      next();
+    });
+  });
 });
 
 module.exports = mongoose.model("User", UserSchema);
